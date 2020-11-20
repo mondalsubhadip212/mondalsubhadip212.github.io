c27386eb9f4ed56105ec5ee82b5b411699902deb
mondalsubhadip212 <74086329+mondalsubhadip212@users.noreply.github.com>
2020-11-20 14:45:41 +0530

    create a front page for Form CRUD operation :(

---
+++ Js/main.js
@@ -0,0 +1,66 @@
+$(document).ready(function () {
+
+    $('#top_card_button').on('click', function click() {
+
+        setTimeout(function (){
+            const dfd = $.ajax({
+
+            url: 'http://127.0.0.1:8000/form/',
+            type: 'GET',
+            dataType: 'json',
+        })
+
+        dfd.done(function (data){
+            table(data)
+        })
+
+        dfd.fail(function () {
+            alert('Something Went Wrong :(')
+        })
+        },2000)
+
+    })
+
+    $('#submit').on('click', function () {
+
+        $.getScript("../Js/serializeToJson.js", function () {
+            const raw_data = $('#form').serializeJSON()
+
+            const dfd = $.ajax({
+                url: 'http://127.0.0.1:8000/form/',
+                type: 'POST',
+                data: raw_data,
+                contentType: 'application/x-www-form-urlencoded; charset=UTF-8',
+                dataType: 'json',
+            })
+
+            dfd.done(function (data) {
+                table(data)
+            })
+
+            dfd.fail(function () {
+                alert('Something Went Wrong :(')
+            })
+        })
+    })
+
+    function table(data) {
+
+        $('#table').load('../Html/table.html', function () {
+            $.each(data, function (index, value) {
+                $('#table_bottom').append('    <tr id="table-body">\n' +
+                    '        <td id="table-body-one">\n' +
+                    value['name'] + '\n' +
+                    '        </td>\n' +
+                    '        <td id="table-body-two">\n' +
+                    value['age'] + '\n' +
+                    '        </td>\n' +
+                    '        <td id="table-body-three">\n' +
+                    value['place'] + '\n' +
+                    '        </td>\n' +
+                    '    </tr>')
+            })
+        })
+    }
+
+})
